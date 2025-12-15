f0ea115ff9c4ecafda62d08518a5b24b270eb07f
Aman Sreejesh <98895109+AmanSreejesh@users.noreply.github.com>
2025-12-14 21:53:23 -0500

    Delete patentsearch/__init__.py

--- patentsearch/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Package entry for patentsearch pipeline.
-
-Exports a simple `run` entry so `patentsearch_pipeline.py` can remain a thin wrapper.
-"""
-from .pipeline import run_patentsearch_pipeline, pretty_print_result
-
-__all__ = ["run_patentsearch_pipeline", "pretty_print_result"]
